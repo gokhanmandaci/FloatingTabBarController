2a2f7c1cdbed044db1fef77b699e12eb363bd72f
Gökhan Mandacı <gokhan.mandaci@etstur.com>
2023-11-20 16:08:40 +0300

    updating config

--- Sources/FloatingTabBarController/Config.swift
+++ Sources/FloatingTabBarController/Config.swift
@@ -13,5 +13,14 @@ final class Config {
     private init() {}
     
     // MARK: - Configuration Parameters
-    
+    /// Custom tab bar height
+    var height: CGFloat = 64
+    /// Bottom spacing of custom tab bar
+    var bottomSpacing: CGFloat = 15
+    /// Minimum tab bar item width
+    var minimumItemWidth: CGFloat = 78
+    /// Total horizontal spacing15 left and 15 right
+    var horizontalSpacing: CGFloat = 30
+    /// Total inner horizontal spacing 18 left and 18 right (Minimum also)
+    var innerHorizontalSpacing: CGFloat = 36
 }
