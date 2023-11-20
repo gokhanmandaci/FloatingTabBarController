276938274021db8b99f6d550abb92f2d6cdc49f5
Gökhan Mandacı <gokhan.mandaci@etstur.com>
2023-11-20 16:30:18 +0300

    updating config

--- Sources/FloatingTabBarController/FloatingTabBarController.swift
+++ Sources/FloatingTabBarController/FloatingTabBarController.swift
@@ -24,10 +24,6 @@ class FloatingTabBarController: UITabBarController {
         
         setImageInsets()
     }
-    
-    func setConfig() {
-        Config.shared.height = 90
-    }
 }
 
 // MARK: - Methods
